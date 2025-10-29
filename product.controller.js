e0f5b01b44290d17312dcec375441b9dde75fa62
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-10-29 10:25:54 +0530

    Create product.controller.js

---
+++ product.controller.js
@@ -0,0 +1,42 @@
+const Product = require("../models/product.model");
+
+// CREATE
+exports.createProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const saved = await product.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// READ
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// UPDATE
+exports.updateProduct = async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// DELETE
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deleted = await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Product deleted", product: deleted });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
